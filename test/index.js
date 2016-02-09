90de10f17033de3d95d827199fe9dc651a251ed3
Azat Mardan <hi@azat.co>
2016-02-09 08:20:10 -0800

    add changes :kimono:

--- test/index.js
+++ test/index.js
@@ -1,26 +1,60 @@
 var expect = require('chai').expect,
-  cp = require('child_process')
+  cp = require('child_process'),
+  superagent = require('superagent'),
+  fs = require('fs'),
+  path = require('path')
+
+var child
 
 
-describe('node version', function () {
-  it('must be 5.1', function(done){
-    expect(process.versions.node).to.equal('5.1.0')
-    done()
-  })
-
-})
-
-describe('npm version', function () {
-  it('must be 2.14.15 or greater', function(done){
-    child = cp.exec('npm -v',
+describe('server', function () {
+  it('must run', function(done){
+    child = cp.exec('node server.js',
     function (error, stdout, stderr) {
       expect(stderr).to.equal('')
       if (error !== null) {
         console.log('exec error: ' + error)
       }
-      stdout = stdout.replace('\n','')
-      // expect(semver.satisfies(stdout, '>=2.14.15')).to.equal(true)
-      done()
+
     })
+    setTimeout(function(){
+      done()
+    }, 400)
   })
+})
+
+describe('server', function () {
+  it('must respond 404 for README-FAKE.md', function(done){
+    superagent
+      .get('http://localhost:3000/README-FAKE.md')
+      .end(function(error, response){
+        expect(response.res.text).to.equal('404 Not Found\n')
+        expect(response.res.statusCode).to.equal(404)
+        done()
+      })
+  })
+})
+
+
+describe('server', function () {
+  it('must respond 200 for README.md', function(done){
+    var readme = fs.readFileSync(path.join(__dirname, '..', 'README.md'), {encoding: 'utf-8'})
+    superagent
+      .get('http://localhost:3000/README.md')
+      .end(function(error, response){
+        expect(response.res.text).to.equal(readme)
+        expect(response.res.statusCode).to.equal(200)
+        // expect(response.res.headers['content-type']).to.equal('text/plain')
+        // expect(response.res.headers['content-length']).to.equal(readme.length)
+        var killResult = child.kill()
+        expect(killResult).to.be.true
+        done()
+      })
+  })
+})
+
+after(function(){
+  if (typeof killResult == 'undefined') {
+    child.kill()
+  }
 })
